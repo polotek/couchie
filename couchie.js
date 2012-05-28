92eb4e683d9523d07f52356ff0dbc0cf60e7490f
Marco Rogers <marco.rogers@gmail.com>
2012-05-27 21:01:19 -0700

    composed to use storage lib

--- couchie.js
+++ couchie.js
@@ -7,14 +7,14 @@
   function Couchie (name) {
     if (name.indexOf('__') !== -1) throw new Error('Cannot have double underscores in name')
     this.name = name
-    this.n = '_couchie__'+name+'__'
+    this.storage = new Storage('_couchie__'+name+'__')
   }
   Couchie.prototype.clear = function (cb) {
-    if (localStorage[this.n+'_revs']) {
+    if (this.storage.has('_revs')) {
       for (var i in this.revs()) {
-        localStorage.removeItem(this.n+i)
+        this.storage.del(i)
       }
-      localStorage.removeItem(this.n+'_revs')
+      this.storage.del('_revs')
       setTimeout(cb, 0)
     } else {
       setTimeout(cb, 0)
@@ -24,7 +24,7 @@
   Couchie.prototype.post = function (obj, cb) {
     if (!obj._id || !obj._rev) return cb(new Error('Document does not have _id or _rev.'))
     var revs = this.revs()
-    localStorage.setItem(this.n+obj._id, JSON.stringify(obj))
+    this.storage.set(obj._id, obj)
     revs[obj._id] = obj._rev
     this.setrevs(revs)
     cb(null)
@@ -34,28 +34,28 @@
     for (var i=0;i<docs.length;i++) {
       var obj = docs[i]
       if (!obj._id || !obj._rev) return cb(new Error('Document does not have _id or _rev.'))
-      localStorage.setItem(this.n+obj._id, JSON.stringify(obj))
+      this.storage.set(obj._id, obj)
       revs[obj._id] = obj._rev
     }
     this.setrevs(revs)
     cb(null)
   }
   Couchie.prototype.get = function (id, cb) {
-    var doc = localStorage.getItem(this.n+id)
+    var doc = this.storage.get(id)
     if (!doc) return cb(new Error('No such doc.'))
-    cb(null, JSON.parse(doc))
+    cb(null, doc)
   }
   Couchie.prototype.all = function (cb) {
     var self = this
     var revs = self.revs()
-    cb(null, Object.keys(revs).map(function (id) {return JSON.parse(localStorage.getItem(self.n+id))}))
+    cb(null, Object.keys(revs).map(function (id) {return self.storage.get(id)}))
   }
   
   Couchie.prototype.revs = function () {
-    return JSON.parse(localStorage.getItem(this.n+'_revs') || '{}')
+    return this.storage.get('_revs') || {}
   }
   Couchie.prototype.setrevs = function (obj) {
-    localStorage.setItem(this.n+'_revs', JSON.stringify(obj))
+    this.storage.set('_revs', obj)
   }
   
   window.couchie = function (name) { return new Couchie(name) }
